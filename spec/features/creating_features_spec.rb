95e16cbc9ba3bf5309830bd09d02eb642fbc9e4d
Alex <avlonitis@msn.com>
2016-03-29 22:37:32 +0100

    added projects crud and validations with TDD

--- spec/features/creating_features_spec.rb
+++ spec/features/creating_features_spec.rb
@@ -1,12 +1,27 @@
 require 'rails_helper'
 
 feature "Users can create new projects" do
-  scenario 'with valid attributes' do
+
+  before do
     visit '/'
     click_link 'New Project'
+  end
+
+  scenario 'with valid attributes' do
     fill_in "Name", with: "Atom"
     fill_in "Description", with: "A text editor for everyone"
     click_button 'Create Project'
     expect(page).to have_content 'Project has been created.'
+    project = Project.find_by(name: "Atom")
+    expect(page.current_url).to eq project_url(project)
+    title = "Atom - Projects - Ticketee"
+    expect(page).to have_title title
   end
+
+  scenario 'when providing invalid attributes' do
+    click_button 'Create Project'
+    expect(page).to have_content "Project has not been created"
+    expect(page).to have_content "Name can't be blank"
+  end
+
 end
