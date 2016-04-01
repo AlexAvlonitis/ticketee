8966c048f4d8ad13d57514139c9c9927d70d4f45
Alex <avlonitis@msn.com>
2016-04-01 17:46:47 +0100

    tickets can be edited

---
+++ spec/features/tickets/editing_tickets_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+feature 'Users can edit existing tickets' do
+
+  let(:project) { FactoryGirl.create(:project) }
+  let(:ticket) { FactoryGirl.create(:ticket, project: project) }
+
+  before do
+    visit project_ticket_path(project, ticket)
+    click_link "Edit Ticket"
+  end
+
+  scenario 'with valid attributes' do
+    fill_in "Name", with: "at editor"
+    click_button "Update Ticket"
+
+    expect(page).to have_content "Ticket has been updated."
+    within("#ticket h2") do
+      expect(page).to have_content "at editor"
+      expect(page).not_to have_content "atom"
+    end
+  end
+
+end
