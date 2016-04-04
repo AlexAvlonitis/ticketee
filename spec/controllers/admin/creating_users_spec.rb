6650220a5f81456a112a9252e3344163a0c9a8a7
Alex <avlonitis@msn.com>
2016-04-04 12:11:30 +0100

    added the ability to add user through the admin backend

---
+++ spec/controllers/admin/creating_users_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+feature 'Admins can create new users' do
+
+  let(:admin) { FactoryGirl.create(:user, :admin) }
+
+  before do
+    login_as(admin)
+    visit '/'
+    click_link 'Admin'
+    click_link 'Users'
+    click_link 'New User'
+  end
+
+  scenario 'with valid credentials' do
+    fill_in "Email", with: "newbie@asd.com"
+    fill_in "Password", with: "asdasdasd"
+    click_button 'Create User'
+    expect(page).to have_content 'User has been created.'
+  end
+
+end
