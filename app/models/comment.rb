d995b1d8886d4c358005c90d7c0112b87be67c10
alex <avlonitis@msn.com>
2016-04-12 17:22:43 +0100

    Tickets can have a state assigned when comments are created

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,10 +1,20 @@
 class Comment < ActiveRecord::Base
   belongs_to :ticket
   belongs_to :author, class_name: 'User'
+  belongs_to :state
 
   scope :persisted, lambda { where.not(id: nil) }
-  
+
   delegate :project, to: :ticket
 
+  after_create :set_ticket_state
+
   validates :text, presence: true
+
+  private
+
+  def set_ticket_state
+    ticket.state = state
+    ticket.save!
+  end
 end
